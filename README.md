4f4eff09a6872d14944cec040e12d440435c46e9
imurzaliev <imurzalievi@gmial.com>
2022-02-17 18:07:43 +0300

    Remove players element and change styling

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Tic-Tac_Toe
+
+The Odin Project assignment. Main goal is to use factory and/or module patterns, and have as little global code as possible.
--- app.js
+++ app.js
@@ -1,32 +1,26 @@
 // GameBoard
-const gameBoard = () => {};
+const gameBoard = (() => {
+  let arr = [];
+  let counter = 2;
 
-const grid = document.querySelector(".board");
-for (i = 0; i < 9; i++) {
-  const div = document.createElement("div");
-  div.classList.add("grid");
-  grid.appendChild(div);
-}
-
-let counter = 2;
-
-const cells = document.querySelectorAll(".grid");
-cells.forEach((cell) => {
-  cell.addEventListener(
-    "click",
-    () => {
-      const xo = document.createElement("h1");
-      if (counter % 2 === 0) {
-        xo.textContent = "X";
-      } else {
-        xo.textContent = "O";
-      }
-      counter++;
-      cell.appendChild(xo);
-    },
-    { once: true }
-  );
-});
+  const cells = document.querySelectorAll(".grid");
+  cells.forEach((cell) => {
+    cell.addEventListener(
+      "click",
+      () => {
+        const xo = document.createElement("h1");
+        if (counter % 2 === 0) {
+          xo.textContent = "X";
+        } else {
+          xo.textContent = "O";
+        }
+        counter++;
+        cell.appendChild(xo);
+      },
+      { once: true }
+    );
+  });
+})();
 
 // Players
 const player = (name) => {};
